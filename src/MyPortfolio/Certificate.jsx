bbdb83d47c86ae8eac53db576004bf92d8cda52d
Xekiee <malaymaxxelmorgan@gmail.com>
2025-10-02 14:13:09 +0800

    Certificate Section

--- src/MyPortfolio/Certificate.jsx
+++ src/MyPortfolio/Certificate.jsx
@@ -1,18 +1,28 @@
 import Styles from "./certificate.module.css";
+import Jira from "../public/JiraCertificate.pdf";
+import SoftwareTesting from "../public/SoftwareTesting.pdf";
+import Katalon from "../public/KatalonApiTesting.pdf";
+import CompleteQACourse from "../public/CompleteQACourse.pdf";
 export default function Certificate() {
   return (
     <div>
-      <section className={Styles.certsec} id="Cerficate">
+      <section id="Cerficate">
         <div className={Styles.title}>
           <span>Certificate</span>
         </div>
         <div className={Styles.certificateProof}>
           <div className={Styles.qaCert}>
-            <div>Morgan</div>
+            <iframe src={Jira} width="100%" height="250px"></iframe>
           </div>
-          <div className={Styles.qaCert}>Morgan</div>
-          <div className={Styles.qaCert}>Morgan</div>
-          <div className={Styles.qaCert}>Morgan</div>
+          <div className={Styles.qaCert}>
+            <iframe src={SoftwareTesting} width="100%" height="250px"></iframe>
+          </div>
+          <div className={Styles.qaCert}>
+            <iframe src={Katalon} width="100%" height="250px"></iframe>
+          </div>
+          <div className={Styles.qaCert}>
+            <iframe src={CompleteQACourse} width="100%" height="250px"></iframe>
+          </div>
         </div>
       </section>
     </div>
